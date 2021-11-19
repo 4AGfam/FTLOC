05bcad0e3409d8d79609e7399d8b3136b223f799
Antonio <antonio.l.george42@gmail.com>
2021-11-18 23:04:09 -0500

    Added star question

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
 'use strict'
 
-let userName = prompt("What is your name?");
 
-console.log(userName);
+function createGreeting(){
+  let userName = getUserName();
+  let greeting = monthOfYear();
+  document.write(greeting + userName + "!");
+ }
 
-if(userName.toLowerCase() == (userName)){
-     alert(userName)
-} else{
-    alert("Happy Holidays "+ (userName));
+ function getUserName(){
+  let userName = prompt("What is your name?");
+  return userName;
 }
 
+//if(userName.toLowerCase() == (userName)){
+//     alert(userName)
+//} else{
+//    alert("Happy Holidays "+ (userName));
+//}
+
+function monthOfYear(){
 let currentMonth = prompt("What is the current month?(1-12)");
 let greeting = "";
-
 if(currentMonth >= 11 && currentMonth <= 12){
-  greeting = "Merry Christmas All!";
+  greeting = "Merry Christmas ";
   alert(greeting) 
 } else if(currentMonth >= 8 && currentMonth <= 10){
-  greeting = "It's so close to Christmas I can taste the Egg Nog!";
+  greeting = "It's so close to Christmas I can taste the Egg Nog ";
   alert(greeting)
 } else if(currentMonth > 0) {
-  greeting = "Is it Christmas yet?!";
+  greeting = "Is it Christmas yet ";
   alert(greeting)
 } else {
-  greeting = "All Jolly, All the Time!";
+  greeting = "All Jolly, All the Time ";
   alert(greeting)
 }
 
-let age = prompt("How old are??");
-let answer = "";
-
-if(currentMonth >= 20 && currentMonth <= 200){
-  answer = "No more freebies for you. But on the bright side you can still watch an awesome movie!";
-  alert(answer) 
-} else if(currentMonth >= 13 && currentMonth <= 19){
-  answer = "Enjoy the freebies while they last and check out a movie!";
-  alert(answer)
-} else if(currentMonth > 0) {
-  answer = "Don't forget to make that Christmas list. Maybe a good movie will give you some ideas!";
-  alert(answer)
-} else {
-  answer = "You must be ancient like Santa Claus!!!";
-  alert(greeting)
+  return greeting;
 }
 
-document.write("Hello " + userName.toUpperCase() + "!");
+function paragraphText(){
+  let answer = ageOfUser();
+  return answer;
+}
+
+//function ageOfUser(){
+//let currentAge = prompt("How old are??");
+//let answer = "";
+//if(currentAge >= 20 && currentAge <= 200){
+//  answer = "No more freebies for you. But on the bright side you can still watch an awesome movie!";
+//  alert(answer) 
+//} else if(currentAge >= 13 && currentAge <= 19){
+//  answer = "Enjoy the freebies while they last and check out a movie!";
+//  alert(answer)
+//} else if(currentAge > 0) {
+//  answer = "Don't forget to make that Christmas list. Maybe a good movie will give you some ideas!";
+//  alert(answer)
+//} else {
+//  answer = "You must be ancient like Santa
+//  Claus!!!";
+//  alert(answer)
+//}
+
+//return answer;
+//}
+
+function displayStars(){
+  let number = prompt("how many stars do you give this website?");
+  for(let star = 1; star <= number; star++ ){
+      document.write('<img src="https://d29fhpw069ctt2.cloudfront.net/clipart/102879/preview/1329328727_preview_8991.png"/>');
+  }
+}
+//document.write("Hello " + userName.toUpperCase() + "!");
